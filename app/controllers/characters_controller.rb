b6bf94de1ace3aa84f642b6de3484e69734e8282
Carlj0666 <carlj1881@yahoo.com>
2020-12-18 13:51:49 -0800

    Added INDEX functionality to display all the characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,6 +1,7 @@
 class CharactersController < ApplicationController
 
   get '/characters' do #THIS IS THE INDEX AND LOADS ALL ARTISTS
+      @characters = Character.all
       erb :'characters/index'
   end
 
