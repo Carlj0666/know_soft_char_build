fca8488e19a0f0a921e525d00b76832f2fddcbdc
Carlj0666 <carlj1881@yahoo.com>
2020-12-20 14:27:17 -0800

    added validates attributes to the User class model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :username, presence: true #validates that username is present
+  validates :username, uniqueness: true
   # has_many :characters
 end
