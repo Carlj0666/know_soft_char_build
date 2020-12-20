63f6f4ab83aac86ac838dae9186acc534bee4bf4
Carlj0666 <carlj1881@yahoo.com>
2020-12-20 14:47:20 -0800

    added validations, created conditional to check for them, started process to show errors if username and pw not present with apppropriate redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,15 @@ class UsersController < ApplicationController
 
   #process form/create user
   post "/signup" do
-    if params[:user][:username] == "" || params[:user][:password] == ""
-      redirect "/signup"
+    user = User.new(params[:user])
+    #we don't make an instance variable becuase we are redirecting which will cause us to lose the var as soon as it redirects
+    if user.save
+      session[:user_id] = user.id
+      redirect "/characters"
     else
-    # binding.pry
-    user = User.create(params[:user])
-    session[:user_id] = user.id
-    redirect "/characters"
+      @errors = user.errors.full_messages
+      #redirecting would cause us to lose access to these errors, instead render the erb
+      erb :"users/new"
     end
   end
 
