5fdfea29ab86988c2c0a47bb216457627ea2ddf4
Carlj0666 <carlj1881@yahoo.com>
2020-12-20 13:34:18 -0800

    beginning setup of sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'top_session' #Look into .env for this(https://github.com/bkeepers/dotenv), not required for proj.
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
 
+  #signup
+
+  #loads signup form
   get "/signup" do
     erb :"/users/new"
+  end
+
+  #process form/create user
+  post "/signup" do
+    user = User.create(params[:user])
+    #binding.pry
   end
 
 end
