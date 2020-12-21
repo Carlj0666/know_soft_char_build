0ad8b506df09a8772f972d02d33093d5a7fae338
Carlj0666 <carlj1881@yahoo.com>
2020-12-21 14:02:59 -0800

    creating post login action in the session controller using authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,18 @@ class SessionsController < ApplicationController
   get "/login" do
     erb :"users/login"
   end
-  
+
+  post "/login" do
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password]) #THE AUTHENTICATE METHOD COMES FROM HAS_SECURE_PASSWORD in the USER model.
+      session[:user_id] = user.id
+      redirect "/characters"
+    else
+      redirect "/login"
+    end
+
+  end
+
   get "/logout" do 
     session.clear
     redirect "/login"
