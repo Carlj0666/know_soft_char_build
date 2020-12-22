dca80c6c338b045a7f6c0a391bdf883420cfdd6a
Carlj0666 <carlj1881@yahoo.com>
2020-12-22 15:55:42 -0800

    Deleted db seed contents to properly reflect functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-# Character.destroy_all
+Character.destroy_all
 
-Character.create(name: "Johnny Silverhand", hometown: "Night City", skill: "Guitar Licks", iconic_item: "Silver Hand")
-Character.create(name: "Jackie Welles", hometown: "Night City", skill: "Runin'", iconic_item: "Book - For Whom The Bell Tolls")
-Character.create(name: "Adam Smasher", hometown: "New York", skill: "Barely Human", iconic_item: "DaiOni Power Armor")
-Character.create(name: "V", hometown: "Night City", skill: "Netrunner", iconic_item: "The Scalpel")
-Character.create(name: "Viktor Vector", hometown: "Watson", skill: "Ripperdoc", iconic_item: "Real Corrective Lenses")
-Character.create(name: "Judy Alvarez", hometown: "Laguna Bend", skill: "Braindance Tech", iconic_item: "DMS Editing Bay")
+# Character.create(name: "Johnny Silverhand", hometown: "Night City", skill: "Guitar Licks", iconic_item: "Silver Hand")
+# Character.create(name: "Jackie Welles", hometown: "Night City", skill: "Runin'", iconic_item: "Book - For Whom The Bell Tolls")
+# Character.create(name: "Adam Smasher", hometown: "New York", skill: "Barely Human", iconic_item: "DaiOni Power Armor")
+# Character.create(name: "V", hometown: "Night City", skill: "Netrunner", iconic_item: "The Scalpel")
+# Character.create(name: "Viktor Vector", hometown: "Watson", skill: "Ripperdoc", iconic_item: "Real Corrective Lenses")
+# Character.create(name: "Judy Alvarez", hometown: "Laguna Bend", skill: "Braindance Tech", iconic_item: "DMS Editing Bay")
